c2e4981b500b060b27a59bc47d288184f1b441a9
Ethan Moles (work computer) <molesa@oregonstate.edu>
2023-12-04 17:39:51 -0800

    overhauled sorter.py which is now functional. needs some improvements but basic functionality is there!

--- sorter.py
+++ sorter.py
@@ -1,46 +1,97 @@
-from tkinter import *
+import os
+import tkinter as tk
+from tkinter import filedialog
 from PIL import Image, ImageTk
-from tkinter.filedialog import askdirectory
-from os import *
 
 
-# initialize Tk app
-root = Tk()
-root.title("Easy Image Sorter")
-root.geometry("800x800")
+class ImageSorterApp:
+    def __init__(self, master):
+        self.master = master  # create root, and create new instance of class with ImageSorterApp(root)
+        self.master.title("Easy Image Sorter")  # title will display at the top of the app
+        self.master.geometry("600x600")  # set the window size to be fixed, so things stay clean and don't move around
 
-# get directory path and create queue
-path = askdirectory(title="Select folder")
-dir_list = listdir(path)
-image_list = []
-for item in dir_list:
-    name = item
-    if ".png" in name:
-        image_list.append(item)
+        # initialize variables
+        self.main_folder = ''
+        self.sort_folders = ['', '', '', '']  # TODO update to take user input for number of folders
+        self.image_list = []  # will store image list from main_folder
+        self.current_index = 0  # index to start iterating through the list
 
-# text_output = Text(root, height=10, width=60)
-# text_output.grid(row=1, column=0, padx=175)
-#
-# for item in image_list:
-#     text_output.insert(END, item + "\n")
+        # create GUI
+        self.create_widgets()  # will run create_widgets as defined below
 
-# pull first image
-image_path = image_list.pop()
-show_image = Image.open(path + "/" + image_path)
-show_image.thumbnail((500, 500), Image.LANCZOS)
-show_image = ImageTk.PhotoImage(show_image)
+    def create_widgets(self):
+        """creates tkinter widgets to display in the master window"""
+        # image label
+        self.image_label = tk.Label(self.master)
+        self.image_label.pack(expand=True)  # pack the image label, and expand to fill space
 
-Label(image=show_image).grid(row=0, column=0, padx=175, pady=50)
+        # 'no more images' label at end of the list
+        self.end_images_label = tk.Label(self.master, text="No more images. Please select a new folder.")
+        self.end_images_label.pack()  # add the label in to the window
+        self.end_images_label.pack_forget()  # hide label initially as it isn't needed until later
+
+        # anchor buttons at the bottom of the window using a frame
+        button_frame = tk.Frame(self.master)
+        button_frame.pack(side=tk.BOTTOM)  # TODO add padding between buttons
+
+        # create buttons for sorting folders
+        select_folders_button = tk.Button(button_frame, text="Select folders", command=self.select_folders)
+        select_folders_button.pack(side=tk.TOP)  # TODO add padding between buttons
+
+        for i in range(4):  # TODO update to take user input for number of folders
+            btn = tk.Button(button_frame, text=f"Move to folder {i + 1}", command=lambda idx=i: self.move_image(idx))
+            # TODO update to show name of folders chosen
+            btn.pack(side=tk.LEFT)
+            # bind key presses
+            self.master.bind(str(i + 1), lambda event, idx=i: self.move_image(idx))
+
+        skip_btn = tk.Button(button_frame, text="SKIP", command=self.next_image)
+        skip_btn.pack(side=tk.LEFT)
+        self.master.bind("5", lambda event: self.next_image())
+
+    def select_folders(self):
+        """selects folders for main_folder and sort_folders"""
+        self.main_folder = filedialog.askdirectory(title="Select main folder")
+        for i in range(4):  # TODO update to have user input for number of folders
+            self.sort_folders[i] = filedialog.askdirectory(title=f"Select folder for button {i+1}")
+
+        # load image list and display first image
+        self.image_list = [f for f in os.listdir(self.main_folder) if
+                           f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+        self.current_index = 0
+        self.load_image()
+
+        # show 'no more images' label when image list is complete
+        self.end_images_label.pack_forget()
+
+    def load_image(self):
+        """loads the image to be displayed and displays it"""
+        if self.current_index < len(self.image_list):
+            image_path = os.path.join(self.main_folder, self.image_list[self.current_index])
+            image = Image.open(image_path)
+            image.thumbnail((400, 400))
+            photo = ImageTk.PhotoImage(image)
+            self.image_label.config(image=photo)  # TODO why is this here?
+            self.image_label.image = photo
+            self.end_images_label.pack_forget()
+        else:
+            self.end_images_label.pack()
+
+    def move_image(self, folder_index):
+        """moves the image to the specified folder based on its index in the folder list"""
+        destination_folder = self.sort_folders[folder_index]
+        current_image_path = os.path.join(self.main_folder, self.image_list[self.current_index])
+        new_image_path = os.path.join(destination_folder, self.image_list[self.current_index])
+        os.rename(current_image_path, new_image_path)
+        self.next_image()
+
+    def next_image(self):
+        """moves to the next image in the list"""
+        self.current_index += 1
+        self.load_image()
 
 
-# button functionality to sort by folder
-
-# display image and buttons
-# test_image = Image.open("C:/Users/US7058/Pictures/Photos/test1.png")
-# test_image.thumbnail((500, 500), Image.LANCZOS)
-# test_image = ImageTk.PhotoImage(test_image)
-#
-# Label(image=test_image).grid(row=0, column=0, padx=175, pady=50)
-Button(root, text="Exit", command=root.quit).grid(row=2, column=0)
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ImageSorterApp(root)
+    root.mainloop()
